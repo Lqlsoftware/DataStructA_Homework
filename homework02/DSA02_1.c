a0a08e93135ca81952bd1dcf8e3df455caa446b9
Robin Lu <lqlsoft@hotmail.com>
2017-03-17 13:07:40 +0800

    Update DSA02_1.c

--- homework02/DSA02_1.c
+++ homework02/DSA02_1.c
@@ -115,7 +115,7 @@ void main() {
             }
 
         }
-        for (int j = 0;j<10;j++) {
+        for (int j = 0;j<i;j++) {
             res[count][j] = sb[j];
             printf(" %d ",sb[j]);
         }
